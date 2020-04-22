0b41c8fca440ee2d64485d06a7ae36236ef10799
Duygu Gözen <nazduyguakyol@gmail.com>
2020-04-22 19:06:05 +0200

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from './App.module.css';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
 	return (
-		<header>
-			<p className={classes.test}>Let the time line magic begin!</p>
-		</header>
+		<BrowserRouter>
+			<header>
+				<p className={classes.test}>Let the time line magic begin!</p>
+			</header>
+		</BrowserRouter>
 	);
 }
 
