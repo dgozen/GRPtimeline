af336b3dcff350343de1ef73de3893707845cee2
Åsa B <backaasa@gmail.com>
2020-04-24 14:24:48 +0200

    added radio button with style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import Style from './App.module.css'
-import RouterComponent from './component/pages/RouterComponent/RouterComponent';
+import React from "react";
+import Style from "./App.module.css";
+import RouterComponent from "./component/pages/RouterComponent/RouterComponent";
+import Filter from "./component/Filter/Filter";
 
 export default function App() {
-	// axios
-	// 	.get('http://www.mocky.io/v2/5ea0850b3200006f0094afb5')
-	// 	.then((response) => {
-	// 		const responseData = response.data.timelineInfo[0];
-	// 		const { category } = responseData;
-	// 		console.log(category);
-	// 	});
-	return (
-		<RouterComponent />
-	)
-};
+  // axios
+  // 	.get('http://www.mocky.io/v2/5ea0850b3200006f0094afb5')
+  // 	.then((response) => {
+  // 		const responseData = response.data.timelineInfo[0];
+  // 		const { category } = responseData;
+  // 		console.log(category);
+  // 	});
+  return (
+    <div>
+      <RouterComponent />
+      <Filter />
+    </div>
+  );
+}
---
+++ src/component/Filter/Filter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import style from "./Filter.module.css"
+
+const Filter = () => {
+    return (
+        <div className={style.filterStyle}>
+            <input type="radio" id="AllCategories" name="Category" value="AllCategories"/>
+            <label for="AllCategories">All Categories</label>
+
+            <input type="radio" id="Publication" name="Category" value="Publication"/>
+            <label for="Publication">Publication</label>
+
+            <input type="radio" id="Trend" name="Category" value="Trend"/>
+            <label for="Trend">Trend</label>
+           
+            <input type="radio" id="Event" name="Category" value="Event"/>
+            <label for="Event">Event</label>
+
+            <input type="radio" id="Program" name="Category" value="Program"/>
+            <label for="Program">Program, platform or investment initiative</label>
+
+            <input type="radio" id="Framework" name="Category" value="Framework"/>
+            <label for="Framework">Framework: conceptual, evaluative, operational</label>
+          
+        </div>
+    );
+};
+
+export default Filter;
