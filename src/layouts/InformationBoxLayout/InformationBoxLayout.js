246f9eb67343b5eaf5ee433e0bf49be6abc74c4d
emelies <emelie.sabel@hyperisland.se>
2020-04-24 13:36:59 +0200

    first steps of the information layout

--- src/layouts/InformationBoxLayout/InformationBoxLayout.js
+++ src/layouts/InformationBoxLayout/InformationBoxLayout.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import style from './InformationBoxLayout.module.css';
-import InformationBox from '../../component/InformationBox/InformationBox';
+import React from "react";
+import style from "./InformationBoxLayout.module.css";
+import InformationBox from "../../component/InformationBox/InformationBox";
 
 const InformationBoxLayout = (props) => {
-	return (
-		<div className={style.infoBoxLayoutStyle}>
-			<InformationBox title='Test 1' text='Some text here' />
-		</div>
-	);
+  return (
+    <div className={style.infoBoxLayoutStyle}>
+      <InformationBox title="Test 1" text="Some text here" />
+      <InformationBox title="Test 1" text="Some text here" />
+      <InformationBox title="Test 1" text="Some text here" />
+      <InformationBox title="Test 1" text="Some text here" />
+      <InformationBox title="Test 1" text="Some text here" />
+      <InformationBox title="Test 1" text="Some text here" />
+    </div>
+  );
 };
 
 export default InformationBoxLayout;
