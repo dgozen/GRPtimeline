644e13f234927a516082c4477f7d7e68ac4f7a5e
Duygu Gözen <nazduyguakyol@gmail.com>
2020-04-24 13:59:48 +0200

    boxes placed in grid

--- src/layouts/InformationBoxLayout/InformationBoxLayout.js
+++ src/layouts/InformationBoxLayout/InformationBoxLayout.js
@@ -1,18 +1,31 @@
-import React from "react";
-import style from "./InformationBoxLayout.module.css";
-import InformationBox from "../../component/InformationBox/InformationBox";
+import React from 'react';
+import style from './InformationBoxLayout.module.css';
+import InformationBox from '../../component/InformationBox/InformationBox';
+// import Auxiliary from '../../hoc/Auxiliary';
 
 const InformationBoxLayout = (props) => {
-  return (
-    <div className={style.infoBoxLayoutStyle}>
-      <InformationBox title="Test 1" text="Some text here" />
-      <InformationBox title="Test 1" text="Some text here" />
-      <InformationBox title="Test 1" text="Some text here" />
-      <InformationBox title="Test 1" text="Some text here" />
-      <InformationBox title="Test 1" text="Some text here" />
-      <InformationBox title="Test 1" text="Some text here" />
-    </div>
-  );
+	return (
+		<div className={style.infoBoxLayoutStyle}>
+			<div className={style.box1}>
+				<InformationBox title='Test 1' text='Some text here' />
+			</div>
+			<div className={style.box2}>
+				<InformationBox title='Test 2' text='Some text here' />
+			</div>
+			<div className={style.box3}>
+				<InformationBox title='Test 3' text='Some text here' />
+			</div>
+			<div className={style.box4}>
+				<InformationBox title='Test 4' text='Some text here' />
+			</div>
+			<div className={style.box5}>
+				<InformationBox title='Test 5' text='Some text here' />
+			</div>
+			<div className={style.box6}>
+				<InformationBox title='Test 6' text='Some text here' />
+			</div>
+		</div>
+	);
 };
 
 export default InformationBoxLayout;
