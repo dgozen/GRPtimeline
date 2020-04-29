5e731c00a192c8b7a85dbed37b9f6728bf3729d2
Åsa B <backaasa@gmail.com>
2020-04-30 00:17:11 +0200

    the checkboxes are now objects that are being mapped out

--- src/component/Filter/Filter.js
+++ src/component/Filter/Filter.js
@@ -6,69 +6,45 @@ const Filter = () => {
     // filter through input boxes, return ID's if selectedCategory is true
   };
 
-  const filterList = [{ type: checkbox }, {}];
+  const filterList = [
+    {
+      id: "AllCategories",
+      label: "All Categories",
+    },
+    {
+      id: "Publication",
+      label: "Publication",
+    },
+    {
+      id: "Trend",
+      label: "Trend",
+    },
+    {
+      id: "Event",
+      label: "Event",
+    },
+
+    {
+      id: "Program",
+      label: "Program, platform or investment initiative",
+    },
+
+    {
+      id: "Framework",
+      label: "Framework: conceptual, evaluative, operational",
+    },
+  ];
 
   //needed: function so allcategories check all boxes on click
 
   return (
     <div className={style.filterStyle}>
-      <input
-        type="checkbox"
-        id="AllCategories"
-        name="Category"
-        value="AllCategories"
-        onChange={setSelectedCategory}
-      />
-      <label for="AllCategories">All Categories</label>
-
-      <input
-        type="checkbox"
-        id="Publication"
-        name="Category"
-        value="Publication"
-        onChange={}
-      />
-      <label for="Publication">Publication</label>
-
-      <input
-        type="checkbox"
-        id="Trend"
-        name="Category"
-        value="Trend"
-        onChange={setSelectedCategory}
-      />
-      <label for="Trend">Trend</label>
-
-      <input
-        type="checkbox"
-        id="Event"
-        name="Category"
-        value="Event"
-        onChange={setSelectedCategory}
-      />
-      <label for="Event">Event</label>
-
-      <input
-        type="checkbox"
-        id="Program"
-        name="Category"
-        value="Program"
-        onChange={setSelectedCategory}
-      />
-      <label for="Program">
-        Program, platform or <br></br>investment initiative
-      </label>
-
-      <input
-        type="checkbox"
-        id="Framework"
-        name="Category"
-        value="Framework"
-        onChange={setSelectedCategory}
-      />
-      <label for="Framework">
-        Framework: conceptual, <br></br> evaluative, operational
-      </label>
+      {filterList.map((data) => (
+        <div>
+          <input type="checkbox" id={data.id}></input>{" "}
+          <label for={data.id}>{data.label}</label>
+        </div>
+      ))}
     </div>
   );
 };
