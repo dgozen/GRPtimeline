8a4f19fb9ed1b3957539daf959b482e99abe5d72
Duygu Gözen <nazduyguakyol@gmail.com>
2020-04-30 14:54:40 +0200

    category state working and being passed around

--- src/component/Filter/Filter.js
+++ src/component/Filter/Filter.js
@@ -1,72 +1,66 @@
-import React, { useState } from "react";
-import style from "./Filter.module.css";
+import React, { useState } from 'react';
+import style from './Filter.module.css';
 
-const Filter = () => {
-  // filter through input boxes, return ID's if selectedCategory is true
-  // const [selectedCategory, setSelectedCategory] = useState();
+const Filter = ({ selectedCategory, setSelectedCategory }) => {
+	// filter through input boxes, return ID's if selectedCategory is true
+	const editCategoryArray = (checked, id) => {
+		const selectedCategoryArray = [...selectedCategory];
+		if (checked) {
+			selectedCategoryArray.push(id);
+		} else if (!checked) {
+			selectedCategoryArray.splice(selectedCategoryArray.indexOf(id), 1);
+		}
+		setSelectedCategory(selectedCategoryArray);
+	};
 
-  const selectedCategoryArray = [];
+	const filterList = [
+		{
+			id: 'AllCategories',
+			label: 'All Categories',
+		},
+		{
+			id: 'Publication',
+			label: 'Publication',
+		},
+		{
+			id: 'Trend',
+			label: 'Trend',
+		},
+		{
+			id: 'Event',
+			label: 'Event',
+		},
 
-  const editCategoryArray = (checked, id) => {
-    if (checked) {
-      selectedCategoryArray.push(id);
-    } else if (!checked) {
-      selectedCategoryArray.splice( selectedCategoryArray.indexOf(id), 1 );
-    }
+		{
+			id: 'Program',
+			label: 'Program, platform or investment initiative',
+		},
 
-    console.log(selectedCategoryArray);
-  };
+		{
+			id: 'Framework',
+			label: 'Framework: conceptual, evaluative, operational',
+		},
+	];
 
-  const filterList = [
-    {
-      id: "AllCategories",
-      label: "All Categories",
-    },
-    {
-      id: "Publication",
-      label: "Publication",
-    },
-    {
-      id: "Trend",
-      label: "Trend",
-    },
-    {
-      id: "Event",
-      label: "Event",
-    },
+	//needed: function so allcategories check all boxes on click
 
-    {
-      id: "Program",
-      label: "Program, platform or investment initiative",
-    },
-
-    {
-      id: "Framework",
-      label: "Framework: conceptual, evaluative, operational",
-    },
-  ];
-
-  //needed: function so allcategories check all boxes on click
-
-  return (
-    <div className={style.filterStyle}>
-      {filterList.map((data) => (
-        <div>
-          <input
-            onChange={(event) => {
-              let checked = event.target.checked;
-              editCategoryArray(checked, data.id);
-              // console.log('look here', setSelectedCategory(...selectedCategoryArray));
-            }
-          }
-            type="checkbox"
-            id={data.id}
-          ></input>{" "}
-          <label for={data.id}>{data.label}</label>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className={style.filterStyle}>
+			{filterList.map((data) => (
+				<div>
+					<input
+						onChange={(event) => {
+							let checked = event.target.checked;
+							editCategoryArray(checked, data.id);
+						}}
+						type='checkbox'
+						id={data.id}
+					></input>{' '}
+					<label for={data.id}>{data.label}</label>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Filter;
--- src/component/pages/DefaultLayout/DefaultLayout.js
+++ src/component/pages/DefaultLayout/DefaultLayout.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import style from "./DefaultLayout.module.css";
-import Header from "../../Header/Header";
-import ButtonArrowSmall from "../../Buttons/ButtonArrowSmall/ButtonArrowSmall";
-import InformationBoxLayout from "../../InformationBoxLayout/InformationBoxLayout";
-import Filter from "../../Filter/Filter";
-import APIFetch from "../../APIFetch/APIFetch";
-import AddButton from "../../Buttons/AddButton/AddButton";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import style from './DefaultLayout.module.css';
+import Header from '../../Header/Header';
+import ButtonArrowSmall from '../../Buttons/ButtonArrowSmall/ButtonArrowSmall';
+import InformationBoxLayout from '../../InformationBoxLayout/InformationBoxLayout';
+import Filter from '../../Filter/Filter';
+import APIFetch from '../../APIFetch/APIFetch';
+import AddButton from '../../Buttons/AddButton/AddButton';
 
 const numbers = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5];
 
@@ -14,31 +14,31 @@ const DefaultLayout = () => {
 	const [scalingState, setScalingState] = useState(0);
 	const [yearsArray, setYearsArrayState] = useState([]);
 	const [clickedYear, setClickedYear] = useState('onload');
-	const [selectedCategory, setSelectedCategory] = useState('AllCategories');
+	const [selectedCategory, setSelectedCategory] = useState([]);
 	const timelineData = APIFetch();
 
-  useEffect(() => {
-    timelineData.then((data) => {
-      let yearsArrays = [];
-      for (let item of data.timelineInfo) {
-        yearsArrays.push(item.year);
-      }
+	useEffect(() => {
+		timelineData.then((data) => {
+			let yearsArrays = [];
+			for (let item of data.timelineInfo) {
+				yearsArrays.push(item.year);
+			}
 
-      setArray(yearsArrays);
-    });
-  }, []);
+			setArray(yearsArrays);
+		});
+	}, []);
 
-  const setArray = (array) => {
-    const passArray = new Set(array);
-    const oneOfEachYear = [...passArray];
-    setYearsArrayState(oneOfEachYear);
-  };
+	const setArray = (array) => {
+		const passArray = new Set(array);
+		const oneOfEachYear = [...passArray];
+		setYearsArrayState(oneOfEachYear);
+	};
 
-  const toggle = () => {
-    setScalingState((scalingState + 1) % numbers.length);
-  };
+	const toggle = () => {
+		setScalingState((scalingState + 1) % numbers.length);
+	};
 
-  const num = numbers[scalingState];
+	const num = numbers[scalingState];
 
 	return (
 		<div>
@@ -64,23 +64,26 @@ const DefaultLayout = () => {
 						</button>
 					))}
 
-          <div className={style.buttonArrowDown}>
-            <ButtonArrowSmall />
-          </div>
-        </div>
-        <div className={style.header}>
-          <Header />
-        </div>
-        <InformationBoxLayout clickedYear={clickedYear} />
-        <div className={style.filter}>
-          <Filter />
-        </div>
-        <div className={style.addbutton}>
-          <AddButton />
-        </div>
-      </div>
-    </div>
-  );
+					<div className={style.buttonArrowDown}>
+						<ButtonArrowSmall />
+					</div>
+				</div>
+				<div className={style.header}>
+					<Header />
+				</div>
+				<InformationBoxLayout clickedYear={clickedYear} />
+				<div className={style.filter}>
+					<Filter
+						selectedCategory={selectedCategory}
+						setSelectedCategory={setSelectedCategory}
+					/>
+				</div>
+				<div className={style.addbutton}>
+					<AddButton />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DefaultLayout;
