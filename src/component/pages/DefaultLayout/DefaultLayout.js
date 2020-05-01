2570a16330fb0b0479ad7d4a7a02ecc4139beb99
Emelie Edman <emelie.edman@hyperisland.se>
2020-05-01 16:04:37 +0200

    fixed the size of each image in the background

--- src/component/pages/DefaultLayout/DefaultLayout.js
+++ src/component/pages/DefaultLayout/DefaultLayout.js
@@ -63,31 +63,156 @@ import y2019 from '../../../assets/2019.svg';
 
 // -  Look up how to change colors on SVG:s
 const backgrounds = {
-  1950: y1950,
-  1973: y1973,
-  1983: y1983,
-  1985: y1985,
-  1992: y1992,
-  1995: y1995,
-  1996: y1996,
-  1999: y1999,
-  2001: y2001,
-  2004: y2004,
-  2005: y2005,
-  2006: y2006,
-  2007: y2007,
-  2008: y2008,
-  2009: y2009,
-  2010: y2010,
-  2011: y2011,
-  2012: y2012,
-  2013: y2013,
-  2014: y2014,
-  2015: y2015,
-  2016: y2016,
-  2017: y2017,
-  2018: y2018,
-  2019: y2019,
+  1950: {
+    year: y1950,
+    width: '185vw',
+    left: '-75vw',
+    top: '-124vh',
+  },
+  1973: {
+    year: y1973,
+    width: '180vw',
+    left: '-72vw',
+    top: '-120vh',
+  },
+  1983: {
+    year: y1983,
+    width: '175vw',
+    left: '-69vw',
+    top: '-118vh',
+  },
+  1985: {
+    year: y1985,
+    width: '170vw',
+    left: '-65vw',
+    top: '-113vh',
+  },
+  1992: {
+    year: y1992,
+    width: '168vw',
+    left: '-64vw',
+    top: '-112vh',
+  },
+  1995: {
+    year: y1995,
+    width: '165vw',
+    left: '-62vw',
+    top: '-112vh',
+  },
+  1996: {
+    year: y1996,
+    width: '163vw',
+    left: '-60vw',
+    top: '-112vh',
+  },
+  1999: {
+    year: y1999,
+    width: '156vw',
+    left: '-56vw',
+    top: '-105vh',
+  },
+  2001: {
+    year: y2001,
+    width: '152vw',
+    left: '-54vw',
+    top: '-98vh',
+  },
+  2004: {
+    year: y2004,
+    width: '152vw',
+    left: '-56vw',
+    top: '-98vh',
+  },
+  2005: {
+    year: y2005,
+    width: '150vw',
+    left: '-56vw',
+    top: '-92vh',
+  },
+  2006: {
+    year: y2006,
+    width: '150vw',
+    left: '-57vw',
+    top: '-100vh',
+  },
+  2007: {
+    year: y2007,
+    width: '147vw',
+    left: '-55vw',
+    top: '-100vh',
+  },
+  2008: {
+    year: y2008,
+    width: '140vw',
+    left: '-50vw',
+    top: '-92vh',
+  },
+  2009: {
+    year: y2009,
+    width: '137vw',
+    left: '-49vw',
+    top: '-84vh',
+  },
+  2010: {
+    year: y2010,
+    width: '128vw',
+    left: '-42vw',
+    top: '-80vh',
+  },
+  2011: {
+    year: y2011,
+    width: '117vw',
+    left: '-34vw',
+    top: '-70vh',
+  },
+  2012: {
+    year: y2012,
+    width: '114vw',
+    left: '-33vw',
+    top: '-68vh',
+  },
+  2013: {
+    year: y2013,
+    width: '110vw',
+    left: '-30vw',
+    top: '-60vh',
+  },
+  2014: {
+    year: y2014,
+    width: '19vw',
+    left: '-30vw',
+    top: '-60vh',
+  },
+  2015: {
+    year: y2015,
+    width: '114vw',
+    left: '-34vw',
+    top: '-64vh',
+  },
+  2016: {
+    year: y2016,
+    width: '110vw',
+    left: '-31vw',
+    top: '-60vh',
+  },
+  2017: {
+    year: y2017,
+    width: '114vw',
+    left: '-38vw',
+    top: '-72vh',
+  },
+  2018: {
+    year: y2018,
+    width: '112vw',
+    left: '-37vw',
+    top: '-71vh',
+  },
+  2019: {
+    year: y2019,
+    width: '113vw',
+    left: '-38vw',
+    top: '-64vh',
+  },
 };
 
 const DefaultLayout = () => {
@@ -133,7 +258,16 @@ const DefaultLayout = () => {
     <div>
       {clickedYear !== '' &&
         yearsToRender.map(([, yearToRender]) => (
-          <img className={style.background} src={yearToRender} alt="" />
+          <img
+            className={style.background}
+            style={{
+              width: yearToRender.width,
+              left: yearToRender.left,
+              top: yearToRender.top,
+            }}
+            src={yearToRender.year}
+            alt=""
+          />
         ))}
       <div className={style.container}>
         <div className={style.timeline}>
@@ -159,7 +293,9 @@ const DefaultLayout = () => {
         <div className={style.header}>
           <Header />
         </div>
-        <InformationBoxLayout clickedYear={clickedYear} />
+        {/* <div className={style.informationBoxLayout}>
+          <InformationBoxLayout clickedYear={clickedYear} />
+        </div> */}
         <div className={style.filter}>
           <Filter />
         </div>
