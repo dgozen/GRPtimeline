f1a8af4c69fe87fe1b54d7db2116f0150a7274d0
essisuomalainen <55558262+essisuomalainen@users.noreply.github.com>
2020-05-03 20:07:33 +0200

    radiobutton fixed names

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -19,11 +19,11 @@ const Form = () => {
             </div>
 
             <div className={style.radioButtons}>
-                <label><input type="radio" value="Investment" name="Investment" />Investment</label>
-                <label><input type="radio" value="Framework" name="Framework" /> Framework</label>
-                <label><input type="radio" value="Event" name="Event" /> Event</label>
-                <label><input type="radio" value="Trend" name="Trend" /> Trend</label>
-                <label><input type="radio" value="Publication" name="Publication" /> Publication</label>
+                <label><input type="radio" value="Investment" name="category" />Investment</label>
+                <label><input type="radio" value="Framework" name="category" /> Framework</label>
+                <label><input type="radio" value="Event" name="category" /> Event</label>
+                <label><input type="radio" value="Trend" name="category" /> Trend</label>
+                <label><input type="radio" value="Publication" name="category" /> Publication</label>
             </div>
             <div className={style.buttonContainer}>
             <div className={style.button}>Submit</div>
