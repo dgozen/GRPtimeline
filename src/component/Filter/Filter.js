e1f76f2e8435a71790340ff0eb3af90a47211236
Duygu Gözen <nazduyguakyol@gmail.com>
2020-05-03 18:09:07 +0200

    fixed scrol bar styling, information box arrows work

--- src/component/Filter/Filter.js
+++ src/component/Filter/Filter.js
@@ -1,136 +1,66 @@
-import React, { useState, useEffect } from "react";
-import style from "./Filter.module.css";
+import React, { useState, useEffect } from 'react';
+import style from './Filter.module.css';
 
 const Filter = ({ selectedCategory, setSelectedCategory }) => {
-  const filterList = [
-    {
-      key: 0,
-      id: "Publication",
-      label: "Publication",
-      select: true,
-    },
-    {
-      key: 1,
-      id: "Trend",
-      label: "Trend",
-      select: true,
-    },
-    {
-      key: 2,
-      id: "Event",
-      label: "Event",
-      select: true,
-    },
+	// filter through input boxes, return ID's if selectedCategory is true
+	const editCategoryArray = (checked, id) => {
+		const selectedCategoryArray = [...selectedCategory];
+		if (checked) {
+			selectedCategoryArray.push(id);
+		} else if (!checked) {
+			selectedCategoryArray.splice(selectedCategoryArray.indexOf(id), 1);
+		}
+		setSelectedCategory(selectedCategoryArray);
+	};
 
-    {
-      key: 3,
-      id: "Program",
-      label: "Program, platform or investment initiative",
-      select: true,
-    },
+	const filterList = [
+		{
+			id: 'AllCategories',
+			label: 'All Categories',
+		},
+		{
+			id: 'Publication',
+			label: 'Publication',
+		},
+		{
+			id: 'Trend',
+			label: 'Trend',
+		},
+		{
+			id: 'Event',
+			label: 'Event',
+		},
 
-    {
-      key: 4,
-      id: "Framework",
-      label: "Framework: conceptual, evaluative, operational",
-      select: true,
-    },
-  ];
+		{
+			id: 'Program',
+			label: 'Program, platform or investment initiative',
+		},
 
-  const [isChecked, setIsChecked] = useState(
-    filterList.map((d) => {
-      return d.select;
-    })
-  );
+		{
+			id: 'Framework',
+			label: 'Framework: conceptual, evaluative, operational',
+		},
+	];
 
-  const [isAllChecked, setIsAllChecked] = useState(true);
+	//needed: function so allcategories check all boxes on click
 
- 
-
-  const toggleChange = (key2) => {
-    setIsChecked(
-      ...filterList.map((d) => {
-        if (key2 === d.key) {
-          setIsAllChecked(false);
-          return d.select;
-        } else {
-          return !d.select
-        }
-      })
-    );
-  };
-
-  const toggleAllChange = () => {
-    setIsAllChecked(!isAllChecked);
-
-    setIsChecked(filterList.map((d) => {
-      d.select = !isAllChecked
-      // pushToArray();
-      
-      return d.select;
-      
-    }));
-   
-  }
-
-
-// const pushToArray = () => {
-//   filterList.map(d => {
-//     const selectedCategoryArray = [...selectedCategory];
-//     if (d.select) {
-//       selectedCategoryArray.push(d.id);
-//     } else if (!d.select) {
-//       selectedCategoryArray.splice(selectedCategoryArray.indexOf(d.id), 1);
-//     }
-//     setSelectedCategory(selectedCategoryArray);
-//   })
-// }
-
-  // filter through input boxes, return ID's if selectedCategory is true
-  const editCategoryArray = (checked, id) => {
-    const selectedCategoryArray = [...selectedCategory];
-    if (checked) {
-      selectedCategoryArray.push(id);
-    } else if (!checked) {
-      selectedCategoryArray.splice(selectedCategoryArray.indexOf(id), 1);
-    }
-   
-
-    setSelectedCategory(selectedCategoryArray);
-  };
-
-  //needed: function so allcategories check all boxes on click
-
-  return (
-    <div className={style.filterStyle}>
-      <input
-        checked={isAllChecked}
-        onChange={() => {
-        toggleAllChange();
-        
-        }}
-        id="AllCategories"
-        type="checkbox"
-      ></input>
-      <label for="AllCategories">All Categories</label>
-
-      {filterList.map((data) => (
-        <div>
-          <input
-            checked={isChecked[data.key]}
-            onChange={(e) => {
-              const checked = e.target.checked;
-              editCategoryArray(checked, data.id);
-              toggleChange(data.key);
-            }}
-            type="checkbox"
-            id={data.id}
-          ></input>{" "}
-          <label for={data.id}>{data.label}</label>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className={style.filterStyle}>
+			{filterList.map((data) => (
+				<div>
+					<input
+						onChange={(event) => {
+							let checked = event.target.checked;
+							editCategoryArray(checked, data.id);
+						}}
+						type='checkbox'
+						id={data.id}
+					></input>{' '}
+					<label for={data.id}>{data.label}</label>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Filter;
--- src/component/InformationBox/InformationBox.js
+++ src/component/InformationBox/InformationBox.js
@@ -4,48 +4,40 @@ import ReadMoreLink from '../Buttons/ReadMoreLink/ReadMoreLink';
 
 const InformationBox = (props) => {
 	let categoryColors = 'red';
-	const passedProp = props.category;
 
-	const categoryColorHandler = (category) => {
-		switch (category) {
-			case 'event':
-				categoryColors = '#0071b8';
-				break;
+	switch (props.category) {
+		case 'event':
+			categoryColors = '#0071b8';
+			break;
 
-			case 'publication':
-				categoryColors = '#33622a';
-				break;
+		case 'publication':
+			categoryColors = '#33622a';
+			break;
 
-			case 'program, platform, investment initiative':
-				categoryColors = '#009ede';
-				break;
-			case 'trend':
-				categoryColors = '#49853e';
-				break;
+		case 'program, platform, investment initiative':
+			categoryColors = '#009ede';
+			break;
+		case 'trend':
+			categoryColors = '#49853e';
+			break;
 
-			case 'framework: conceptual, evaluative, operational':
-				categoryColors = '#58595b';
-				break;
+		case 'framework: conceptual, evaluative, operational':
+			categoryColors = '#58595b';
+			break;
 
-			default:
-				console.log('something is broken');
-		}
-	};
-
-	categoryColorHandler(passedProp);
+		default:
+			console.log('something is broken');
+	}
 
 	return (
 		<div className={style.boxStyle} style={{ backgroundColor: categoryColors }}>
 			<h2>{props.title}</h2>
-			<div style={{ overflow:'auto' }}>
-			<p>{props.text}</p></div>
+			<p>{props.text}</p>
 			<div className={style.linkPlacement}>
 				<ReadMoreLink link={props.link} />
 			</div>
 		</div>
 	);
 };
-
-
 
 export default InformationBox;
