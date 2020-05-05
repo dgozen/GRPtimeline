1dee69a6556a1674587951571758e72a7463ddd7
emelies <emelie.sabel@hyperisland.se>
2020-05-05 12:11:35 +0200

    layout and highlight of year

--- src/component/pages/DefaultLayout/DefaultLayout.js
+++ src/component/pages/DefaultLayout/DefaultLayout.js
@@ -1,122 +1,113 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import style from './DefaultLayout.module.css';
-import Header from '../../Header/Header';
-import ButtonArrowSmall from '../../Buttons/ButtonArrowSmall/ButtonArrowSmall';
-import InformationBoxLayout from '../../InformationBoxLayout/InformationBoxLayout';
-import Filter from '../../Filter/Filter';
-import APIFetch from '../../APIFetch/APIFetch';
-import AddButton from '../../Buttons/AddButton/AddButton';
-import backgrounds from './backgrounds';
-import Form from '../../Form/Form';
+import React, { useCallback } from "react";
+import { useState, useEffect } from "react";
+import style from "./DefaultLayout.module.css";
+import Header from "../../Header/Header";
+import ButtonArrowSmall from "../../Buttons/ButtonArrowSmall/ButtonArrowSmall";
+import InformationBoxLayout from "../../InformationBoxLayout/InformationBoxLayout";
+import Filter from "../../Filter/Filter";
+import APIFetch from "../../APIFetch/APIFetch";
+import AddButton from "../../Buttons/AddButton/AddButton";
+import backgrounds from "./backgrounds";
+import Form from "../../Form/Form";
 
 const DefaultLayout = () => {
-	const [yearsArray, setYearsArrayState] = useState([]);
-	const [clickedYear, setClickedYear] = useState('onload');
-	const [selectedCategory, setSelectedCategory] = useState([]);
-	const timelineData = APIFetch();
+  const [yearsArray, setYearsArrayState] = useState([]);
+  const [clickedYear, setClickedYear] = useState("onload");
+  const [selectedCategory, setSelectedCategory] = useState([]);
+  const timelineData = APIFetch();
 
-	// this function yearsToRender filters the images on the keys < or = clickedYear
-	// and transforms the key and clickedYear from string to integer using parseInt()
-	const yearsToRender = Object.entries(backgrounds).filter(
-		([key]) => parseInt(key) <= parseInt(clickedYear)
-	);
+  // this function yearsToRender filters the images on the keys < or = clickedYear
+  // and transforms the key and clickedYear from string to integer using parseInt()
+  const yearsToRender = Object.entries(backgrounds).filter(
+    ([key]) => parseInt(key) <= parseInt(clickedYear)
+  );
 
-	useEffect(() => {
-		timelineData.then((data) => {
-			let yearsArrays = [];
-			for (let item of data.timelineInfo) {
-				yearsArrays.push(item.year);
-			}
+  useEffect(() => {
+    timelineData.then((data) => {
+      let yearsArrays = [];
+      for (let item of data.timelineInfo) {
+        yearsArrays.push(item.year);
+      }
 
-			setArray(yearsArrays);
-		});
-	}, []);
+      setArray(yearsArrays);
+    });
+  }, []);
 
-	const setArray = (array) => {
-		const passArray = new Set(array);
-		const oneOfEachYear = [...passArray];
-		setYearsArrayState(oneOfEachYear);
-	};
+  const setArray = (array) => {
+    const passArray = new Set(array);
+    const oneOfEachYear = [...passArray];
+    setYearsArrayState(oneOfEachYear);
+  };
 
-	// Add button
-	const [showForm, setShowForm] = useState(false);
+  // Add button
+  const [showForm, setShowForm] = useState(false);
 
+  const clickHandler = () => {
+    setShowForm(!showForm);
+  };
 
+  return (
+    <div>
+      {/* this function maps through the yearsToRender function, and renders all images < = to clickedYear  */}
+      {yearsToRender.map(([, yearToRender]) => (
+        <img
+          className={style.background}
+          style={{
+            width: yearToRender.width,
+            left: yearToRender.left,
+            top: yearToRender.top,
+          }}
+          src={yearToRender.year}
+          alt=""
+        />
+      ))}
+      <div className={style.container}>
+        <div className={style.timeline}>
+          <div className={style.buttonArrowUp}>
+            <ButtonArrowSmall />
+          </div>
+          {yearsArray.map((item, index) => (
+            <button
+              key={index}
+              className={
+                clickedYear === item ? style.buttonClick : style.buttonYear
+              }
+              onClick={() => {
+                setClickedYear(item);
+              }}
+            >
+              {item}
+            </button>
+          ))}
 
-
-	const clickHandler = () => {
-   setShowForm(!showForm);
-   
-	 };
-
-	return (
-		<div>
-			{/* this function maps through the yearsToRender function, and renders all images < = to clickedYear  */}
-			{yearsToRender.map(([, yearToRender]) => (
-				<img
-					className={style.background}
-					style={{
-						width: yearToRender.width,
-						left: yearToRender.left,
-						top: yearToRender.top,
-					}}
-					src={yearToRender.year}
-					alt=''
-				/>
-			))}
-			<div className={style.container}>
-				<div className={style.timeline}>
-					<div className={style.buttonArrowUp}>
-						<ButtonArrowSmall />
-					</div>
-					{yearsArray.map((item, index) => (
-						<button
-							key={index}
-							className={style.buttonYear}
-							onClick={() => {
-								setClickedYear(item);
-							}}
-						>
-							{item}
-						</button>
-					))}
-
-
-					<div className={style.buttonArrowDown}>
-						<ButtonArrowSmall />
-					</div>
-				</div>
-				<div className={style.header}>
-					<Header />
-				</div>
-				<div className={style.informationLayout}>
-					{clickedYear === 'onload' ? null : (
-						<InformationBoxLayout
-							clickedYear={clickedYear}
-							selectedCategory={selectedCategory}
-						/>
-					)}
-				</div>
-				<div className={style.filter}>
-					<Filter
-						selectedCategory={selectedCategory}
-						setSelectedCategory={setSelectedCategory}
-					/>
-				</div>
-				<div className={style.addbutton} onClick={() => clickHandler()}>
-					<AddButton />
-					
-				</div>
-				<div className={style.form}>
-						{ showForm ?
-             			 <Form />
-              			: setShowForm
-             			}
-					</div>
-			</div>
-			</div>
-	);
+          <div className={style.buttonArrowDown}>
+            <ButtonArrowSmall />
+          </div>
+        </div>
+        <div className={style.header}>
+          <Header />
+        </div>
+        <div className={style.informationLayout}>
+          {clickedYear === "onload" ? null : (
+            <InformationBoxLayout
+              clickedYear={clickedYear}
+              selectedCategory={selectedCategory}
+            />
+          )}
+        </div>
+        <div className={style.filter}>
+          <Filter
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+        </div>
+        <div className={style.addbutton} onClick={() => clickHandler()}>
+          <AddButton />
+        </div>
+        <div className={style.form}>{showForm ? <Form /> : setShowForm}</div>
+      </div>
+    </div>
+  );
 };
 
 export default DefaultLayout;
