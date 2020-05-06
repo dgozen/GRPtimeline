0f6eec13819d75a73a231d35126535d1cea3cc05
Eva Thorisdottir <heidrune97@gmail.com>
2020-05-06 16:21:37 +0200

    All selected by default, select/deselect all works as well as selecting individual categories

--- src/component/Filter/Filter.js
+++ src/component/Filter/Filter.js
@@ -1,66 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import style from './Filter.module.css';
+import React, { useState, useEffect } from "react";
+import style from "./Filter.module.css";
 
 const Filter = ({ selectedCategory, setSelectedCategory }) => {
-	// filter through input boxes, return ID's if selectedCategory is true
-	const editCategoryArray = (checked, id) => {
-		const selectedCategoryArray = [...selectedCategory];
-		if (checked) {
-			selectedCategoryArray.push(id);
-		} else if (!checked) {
-			selectedCategoryArray.splice(selectedCategoryArray.indexOf(id), 1);
-		}
-		setSelectedCategory(selectedCategoryArray);
-	};
+  const [allChecked, setAllChecked] = useState(true);
+  // filter through input boxes, return ID's if selectedCategory is true
 
-	const filterList = [
-		{
-			id: 'AllCategories',
-			label: 'All Categories',
-		},
-		{
-			id: 'Publication',
-			label: 'Publication',
-		},
-		{
-			id: 'Trend',
-			label: 'Trend',
-		},
-		{
-			id: 'Event',
-			label: 'Event',
-		},
+  const filterList = [
+    {
+      id: "Publication",
+      label: "Publication",
+      isChecked: true,
+    },
+    {
+      id: "Trend",
+      label: "Trend",
+      isChecked: true,
+    },
+    {
+      id: "Event",
+      label: "Event",
+      isChecked: true,
+    },
 
-		{
-			id: 'Program',
-			label: 'Program, platform or investment initiative',
-		},
+    {
+      id: "Program",
+      label: "Program, platform or investment initiative",
+      isChecked: true,
+    },
 
-		{
-			id: 'Framework',
-			label: 'Framework: conceptual, evaluative, operational',
-		},
-	];
+    {
+      id: "Framework",
+      label: "Framework: conceptual, evaluative, operational",
+      isChecked: true,
+    },
+  ];
+  const [filterListState, setFilterListState] = useState(filterList);
 
-	//needed: function so allcategories check all boxes on click
+  // const updateRenderList = () => {};
 
-	return (
-		<div className={style.filterStyle}>
-			{filterList.map((data) => (
-				<div>
-					<input
-						onChange={(event) => {
-							let checked = event.target.checked;
-							editCategoryArray(checked, data.id);
-						}}
-						type='checkbox'
-						id={data.id}
-					></input>{' '}
-					<label for={data.id}>{data.label}</label>
-				</div>
-			))}
-		</div>
-	);
+  const handleCategoryChange = (checked, categoryName) => {
+    setFilterListState((prevState) => {
+      let filterListState = prevState;
+
+      if (categoryName === "AllCategories") {
+        setAllChecked((prevState) => (prevState = !prevState));
+        filterListState = filterListState.map((category) => ({
+          ...category,
+          isChecked: checked,
+        }));
+      } else {
+        filterListState = filterListState.map((category) =>
+          category.id === categoryName
+            ? { ...category, isChecked: checked }
+            : category
+        );
+        setAllChecked(filterListState.every((category) => category.isChecked));
+      }
+      return filterListState;
+    });
+  };
+
+  return (
+    <div className={style.filterStyle}>
+      <div>
+        <input
+          onChange={(event) => {
+            let checked = event.target.checked;
+            let category = event.target.id;
+
+            handleCategoryChange(checked, event.target.id);
+          }}
+          type="checkbox"
+          id="AllCategories"
+          checked={allChecked}
+        />
+        <label for="AllCategories">All Categories</label>
+      </div>
+      {filterListState.map((data) =>
+        data.id ? (
+          <div>
+            <input
+              onChange={(event) => {
+                let checked = event.target.checked;
+                let category = event.target.id;
+
+                handleCategoryChange(checked, category);
+              }}
+              type="checkbox"
+              id={data.id}
+              checked={data.isChecked}
+            ></input>{" "}
+            <label for={data.id}>{data.label}</label>
+          </div>
+        ) : null
+      )}
+    </div>
+  );
 };
 
 export default Filter;
